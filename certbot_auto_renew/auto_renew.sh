2912002b310e2d4790190d83c889cdb1f8c0e938
Sam Edu <dsparklingsam@gmail.com>
2022-11-03 10:50:41 +0100

    auto-renew

--- certbot_auto_renew/auto_renew.sh
+++ certbot_auto_renew/auto_renew.sh
@@ -1,5 +1,33 @@
-cd /etc/nginx
+#!/bin/bash
+#
+# Obtain cert.
+# certbot-auto certonly
 
-crontab -l
+# Set up daily cron job.
+CRON_SCRIPT="/etc/cron.daily/certbot-renew"
 
-echo "successfully Done"
+cat > "${CRON_SCRIPT}" <<EOF
+#!/bin/bash
+#
+# Renew the Let's Encrypt certificate if it is time. It won't do anything if
+# not.
+#
+# This reads the standard /etc/letsencrypt/cli.ini.
+#
+# May or may not have HOME set, and this drops stuff into ~/.local.
+export HOME="/root"
+# PATH is never what you want it it to be in cron.
+export PATH="\${PATH}:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+certbot-auto --no-self-upgrade certonly
+# If the cert updated, we need to update the services using it. E.g.:
+if service --status-all | grep -Fq 'apache2'; then
+  service apache2 reload
+fi
+if service --status-all | grep -Fq 'httpd'; then
+  service httpd reload
+fi
+if service --status-all | grep -Fq 'nginx'; then
+  service nginx reload
+fi
+EOF
+chmod a+x "${CRON_SCRIPT}"
